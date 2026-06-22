46edb34f864b3b15f5e323b4dd4dad775e0cd3d6
Subhamoy Datta <subhamoydatta99@gmail.com>
2026-06-22 22:03:41 +0530

    feat: initialize separate Redis configurations for BullMQ and general caching

--- backend/src/config/redis.bullmq.ts
+++ backend/src/config/redis.bullmq.ts
@@ -1,7 +1,11 @@
 import IORedis from "ioredis";
 
-export const bullRedisConnection = new IORedis({
-    host: process.env.REDIS_HOST || "localhost",
-    port: Number(process.env.REDIS_PORT) || 6379,
-    maxRetriesPerRequest: null,
-});
+export const bullRedisConnection = process.env.REDIS_URL
+  ? new IORedis(process.env.REDIS_URL, {
+      maxRetriesPerRequest: null,
+    })
+  : new IORedis({
+      host: process.env.REDIS_HOST || "localhost",
+      port: Number(process.env.REDIS_PORT) || 6379,
+      maxRetriesPerRequest: null,
+    });
--- backend/src/config/redis.caching.ts
+++ backend/src/config/redis.caching.ts
@@ -1,9 +1,11 @@
 import { createClient } from "redis";
 
 export const redisClient = createClient({
-  url: `redis://${process.env.REDIS_HOST || "localhost"}:${
-    process.env.REDIS_PORT || "6379"
-  }`,
+  url:
+    process.env.REDIS_URL ||
+    `redis://${process.env.REDIS_HOST || "localhost"}:${
+      process.env.REDIS_PORT || "6379"
+    }`,
 });
 
 redisClient.on("error", (err) => {
@@ -13,7 +15,7 @@ redisClient.on("error", (err) => {
 export async function connectRedis() {
   try {
     await redisClient.connect();
-    
+
     console.log("Redis Connected");
   } catch (error) {
     console.error("Redis Connection Failed:", error);
